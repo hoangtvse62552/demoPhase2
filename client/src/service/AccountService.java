6e28fea4d0cb662cb67a6adf4be9a13b725e5d21
Nguyễn Quang Huy <117385142+nqh99@users.noreply.github.com>
2023-03-17 15:41:50 +0700

    Update account serviece
    change code in login function

--- client/src/service/AccountService.java
+++ client/src/service/AccountService.java
@@ -4,13 +4,8 @@ import main.ServerCfg;
 import model.Account;
 import request.AccountRequest;
 import response.AccountResponse;
-<<<<<<< Updated upstream
-import response.ResponseModel;
-import utils.Utils;
-=======
 import utils.ConnectManager;
 import utils.XmlUtils;
->>>>>>> Stashed changes
 
 public class AccountService
 {
@@ -29,61 +24,19 @@ public class AccountService
 
         // Get connection
         ServerCfg serverCfg = new ServerCfg("127.0.0.1", 9090);
-<<<<<<< Updated upstream
-        try (Socket clientSocket = new Socket(serverCfg.getServerIp(), serverCfg.getServerPort()); PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true); BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
-        {
-
-            // flush data to server
-            writer.print(xmlRq);
-
-            // getting response XML string
-            StringBuilder responseXML = new StringBuilder();
-            String line;
-            String xmlResp = "";
-            while (!"".equals((line = reader.readLine())))
-            {
-                responseXML.append(line).append(System.lineSeparator());
-                xmlResp += line;
-            }
-
-            System.out.println(responseXML);
-            AccountResponse accountResponse = (AccountResponse) util.convertXmlToResponse(xmlResp);
-
-            if ("success".equals(accountResponse.getStatus()))
-            {
-                account.setAdmin(accountResponse.isAdmin());
-                account.setUsername(username);
-                account.setPassword(password);
-            }
-            else
-            {
-                System.out.println(accountResponse.getError());
-                return null;
-
-            }
-=======
         ConnectManager connectManager = new ConnectManager();
 
         AccountResponse accountResponse = (AccountResponse) connectManager.getResponse(serverCfg, xmlRq);
->>>>>>> Stashed changes
 
         if ("success".equals(accountResponse.getStatus()))
         {
             account.setAdmin(accountResponse.isAdmin());
-            System.out.println(accountResponse.toString());
         }
         else
         {
-<<<<<<< Updated upstream
-
-            System.out.println("Server not found: " + ex.getMessage());
-
-=======
             System.out.println(accountResponse.getError());
             return null;
->>>>>>> Stashed changes
         }
-
         return account;
     }
 }
