18605e135615333962d46996f9878775e21a42b5
Nguyễn Quang Huy <117385142+nqh99@users.noreply.github.com>
2023-03-28 17:21:21 +0700

    update
    update

--- server/src/configures/Config.java
+++ server/src/configures/Config.java
@@ -2,20 +2,7 @@ package configures;
 
 public class Config {
 
-    private static final String url = "jdbc:db2://" + "localhost:50002" + "/" + "demo2";
-    private static final String user = "db2inst1";
-    private static final String password = "Nqh1999@";
-
-    public static String getUrl() {
-        return url;
-    }
-
-    public static String getUser() {
-        return user;
-    }
-
-    public static String getPassword() {
-        return password;
-    }
-
+    public static final String URL = "jdbc:db2://" + "localhost:50002" + "/" + "demo2";
+    public static final String USERNAME = "db2inst1";
+    public static final String PASSWORD = "Nqh1999@";
 }
--- server/src/utils/TransactionManager.java
+++ server/src/utils/TransactionManager.java
@@ -5,25 +5,35 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import configures.Config;
-
-public class TransactionManager extends HikariDataSource {
+public class TransactionManager extends HikariDataSource
+{
     private static volatile TransactionManager transactionManager = null;
 
-    private TransactionManager() {
+    private TransactionManager()
+    {
     };
 
-    public static TransactionManager getInstance() {
-        if (transactionManager == null) {
-            synchronized (TransactionManager.class) {
-                if (transactionManager == null) {
-                    transactionManager = new TransactionManager();
-                    transactionManager.setJdbcUrl(Config.getUrl());
-                    transactionManager.setUsername(Config.getUser());
-                    transactionManager.setPassword(Config.getPassword());
-                    transactionManager.setDataSourceClassName("com.ibm.db2.jcc.DB2SimpleDataSource");
+    public static TransactionManager getInstance()
+    {
+        if (transactionManager == null)
+        {
+            synchronized (TransactionManager.class)
+            {
+                if (transactionManager == null)
+                {
+                    
+                    HikariConfig config = new HikariConfig();
+                    config.setJdbcUrl("jdbc:db2://" + "localhost:50002" + "/" + "demo2");
+                    config.setUsername("db2inst1");
+                    config.setPassword("Nqh1999@");
+                    // transactionManager.setJdbcUrl();
+                    // transactionManager.setUsername("db2inst1");
+                    // transactionManager.setPassword("Nqh1999@");
+                    // transactionManager.setDataSourceClassName("com.ibm.db2.jcc.DB2SimpleDataSource");
+                    transactionManager = new TransactionManager(config);
                     transactionManager.setMaximumPoolSize(10);
                     transactionManager.setAutoCommit(false);
                 }
@@ -32,19 +42,26 @@ public class TransactionManager extends HikariDataSource {
         return transactionManager;
     }
 
-    public void closeConnection(Connection con, PreparedStatement stm, ResultSet rs) {
-        try {
-            if (rs != null) {
+    public void closeConnection(Connection con, PreparedStatement stm, ResultSet rs)
+    {
+        try
+        {
+            if (rs != null)
+            {
                 rs.close();
             }
-            if (stm != null) {
+            if (stm != null)
+            {
                 stm.close();
             }
-            if (con != null) {
+            if (con != null)
+            {
                 con.commit();
                 con.close();
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
     }
